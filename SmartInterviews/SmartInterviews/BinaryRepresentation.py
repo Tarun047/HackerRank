e86b4d28f002b4657428d1b46cfab2f0c4eb951e
Tarun047 <gtarun047@gmail.com>
2018-09-18 18:51:27 +0530

    Updated proper code without using bin built-in

--- SmartInterviews/SmartInterviews/BinaryRepresentation.py
+++ SmartInterviews/SmartInterviews/BinaryRepresentation.py
@@ -1,4 +1,14 @@
+from math import log
 t = int(input())
 while t:
-    print(bin(int(input()))[2:])
+    n = int(input())
+    if n==0:
+        print(0)
+        t-=1
+        continue
+    ans = []
+    for i in range(0,int(log(n,2))+1):
+        ans += str(n&1)
+        n>>=1
+    print(''.join(reversed(ans)))
     t-=1
